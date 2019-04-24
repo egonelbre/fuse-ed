030e74e9dc61a4f394d60cb105fc2747890caad3
Egon Elbre <egonelbre@gmail.com>
2019-04-24 22:42:37 +0300

    fix typos

--- static/js/canvas.js
+++ static/js/canvas.js
@@ -1,22 +1,22 @@
 "use strict";
 
-var tau = Math.PI*2;
+var tau = Math.PI * 2;
 
-var angle_between = function(from, to){
-	var dx = from.x - to.x,
-		dy = from.y - to.y;
-	return Math.atan2(dy, dx);
-}
+function angle_between(from, to) {
+    var dx = from.x - to.x,
+        dy = from.y - to.y;
+    return Math.atan2(dy, dx);
+};
 
-CanvasRenderingContext2D.prototype.arrow = function(points, width, lineWidth){
-    if(points.length < 2){
-    	throw "Requires at least 2 points!";
+CanvasRenderingContext2D.prototype.arrow = function (points, width, lineWidth) {
+    if (points.length < 2) {
+        throw "Requires at least 2 points!";
     }
-    
+
     lineWidth = lineWidth || 1;
 
     var fill = ctx.fillStyle,
-    	stroke = ctx.strokeStyle;
+        stroke = ctx.strokeStyle;
 
     var angle, dx, dy, prex, prey, last;
 
@@ -26,24 +26,24 @@ CanvasRenderingContext2D.prototype.arrow = function(points, width, lineWidth){
 
     this.beginPath();
     this.moveTo(points[0].x, points[0].y);
-    for(var i = 1; i < points.length-1; i += 1){
-    	this.lineTo(points[i].x, points[i].y);
+    for (var i = 1; i < points.length - 1; i += 1) {
+        this.lineTo(points[i].x, points[i].y);
     }
 
-    last = points[points.length-1];
+    last = points[points.length - 1];
 
-    angle = angle_between(points[points.length-2], last);
-    dx = Math.cos(angle)*width*2;
-    dy = Math.sin(angle)*width*2;
+    angle = angle_between(points[points.length - 2], last);
+    dx = Math.cos(angle) * width * 2;
+    dy = Math.sin(angle) * width * 2;
 
     prex = last.x + dx,
-    prey = last.y + dy;
-    
+        prey = last.y + dy;
+
     this.lineTo(prex, prey);
     this.stroke();
 
     this.strokeStyle = fill;
-    this.lineWidth = width - lineWidth*2;
+    this.lineWidth = width - lineWidth * 2;
     this.stroke();
 
     this.strokeStyle = stroke;
@@ -53,49 +53,48 @@ CanvasRenderingContext2D.prototype.arrow = function(points, width, lineWidth){
     this.lineJoin = "miter";
 
     this.beginPath();
-    
-    angle += tau/4;
-    dx = Math.cos(angle)*width;
-    dy = Math.sin(angle)*width;
-    
-    this.moveTo(prex+dx, prey+dy);
+
+    angle += tau / 4;
+    dx = Math.cos(angle) * width;
+    dy = Math.sin(angle) * width;
+
+    this.moveTo(prex + dx, prey + dy);
     this.lineTo(last.x, last.y);
-    this.lineTo(prex-dx, prey-dy);
+    this.lineTo(prex - dx, prey - dy);
 
     this.closePath();
-	this.fill();
+    this.fill();
     this.stroke();
-}
+};
 
-
-(function() {
 // http://paulirish.com/2011/requestanimationframe-for-smart-animating/
 // http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
- 
+
 // requestAnimationFrame polyfill by Erik Möller
 // fixes from Paul Irish and Tino Zijdel
- 
-(function() {
+
+(function () {
     var lastTime = 0;
     var vendors = ['ms', 'moz', 'webkit', 'o'];
-    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
-                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
+    for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame']
+            || window[vendors[x] + 'CancelRequestAnimationFrame'];
     }
- 
+
     if (!window.requestAnimationFrame)
-        window.requestAnimationFrame = function(callback, element) {
+        window.requestAnimationFrame = function (callback, element) {
             var currTime = new Date().getTime();
             var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
-              timeToCall);
+            var id = window.setTimeout(function () { callback(currTime + timeToCall); },
+                timeToCall);
             lastTime = currTime + timeToCall;
             return id;
         };
- 
-    if (!window.cancelAnimationFrame)
-        window.cancelAnimationFrame = function(id) {
+
+    if (!window.cancelAnimationFrame) {
+        window.cancelAnimationFrame = function (id) {
             clearTimeout(id);
         };
-}());
+    }
+})();
